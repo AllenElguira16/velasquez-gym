588238ef65dc93a906a28559060c77bb48aa6ce4
Michael Allen Elguira <AllenElguira16@gmail.com>
2021-10-24 20:02:16 +0800

    Initial Commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>Tres Maria's Store</title>
+
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    </head>
+    <body>
+        <div>
+            Home Page
+        </div>
+        <script src="{{ mix('js/app.js') }}"></script>
+    </body>
+</html>
