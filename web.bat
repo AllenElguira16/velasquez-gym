d170e588f0673badb71911fe6afc9abf6b2c09f0
Allen Elguira <AllenElguira16@gmail.com>
2022-01-10 18:00:12 +0800

    Fix snackbar

--- web.bat
+++ web.bat
@@ -1,8 +1,8 @@
 @echo off
 cls
 IF exist web/node_modules (
-	cd web && npm run start
+	cd web && npm run build && npm run start
 ) ELSE (
 	echo Installing Backend Modules
-	cd web && npm install && npm run start
+	cd web && npm install && npm run build && npm run start
 )
